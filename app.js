6de20472c042fa7b02c88489bea1d8d81e855515
nightbomd <antfel320@gmail.com>
2025-11-12 00:36:23 +0000

    Tried to loop but didint work so I deleted it

--- app.js
+++ app.js
@@ -1,19 +1,16 @@
 
-
 const properties = {
-   userName: window.prompt("What is your Name?"),
-   location: window.prompt("pick a random location"),
-   biggestOpp: window.prompt("who do you hate the most?"),
-   description: window.prompt("How would you describe yourself in one word?"),
-   year: window.prompt("the year"),
-   advocate: window.prompt("what do you believe most in?"),
-   antiAdvocate: window.prompt("what do you think should be reformed the most in the current state of the world?"),
-   profession: window.prompt("what do you wanna be when you grow up?"),
-}
+  userName: window.prompt("What is your name?"),
+  location: window.prompt("Pick a random location"),
+  biggestOpp: window.prompt("Who do you hate the most?"),
+  description: window.prompt("How would you describe yourself in one word?"),
+  year: window.prompt("The year"),
+  advocate: window.prompt("What do you believe most in?"),
+  antiAdvocate: window.prompt("What do you think should be reformed the most in the current state of the world?"),
+  profession: window.prompt("What do you wanna be when you grow up?")
+};
 
-properties.forEach(property => {
-   property.classList.add("property")
-})
+
 
 const story = `${properties.userName}, who is a ${properties.profession} and a strident advocate for the cause of ${properties.advocate}, in their debate, is fighting against ${properties.biggestOpp}, in which ${properties.userName} is condemnding ${[properties.antiAdvocate]}. In their speech, located at ${properties.location} ${properties.userName} asserts, "It must be noted that ${properties.advocate} is the right cause on the basis of moral judgement and well-being, and that ${properties.biggestOpp}'s beliefs of ${properties.antiAdvocate} is wrong, and they should be publicaly shamed.`;
 
