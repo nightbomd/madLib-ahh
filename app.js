074a67698e322308aaed722e4c5c0a8c807a5cde
nightbomd <antfel320@gmail.com>
2025-11-12 03:35:20 +0000

    addded comments

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
 
+// mad lib prompts/properties
 const properties = {
-  userName: window.prompt("What is your name?"),
+  userName: window.prompt("What is your chosen name?"),
   location: window.prompt("Pick a random location"),
   biggestOpp: window.prompt("Who do you hate the most?"),
   description: window.prompt("How would you describe yourself in one word?"),
-  year: window.prompt("The year"),
+  year: window.prompt("The chosen year"),
   advocate: window.prompt("What do you believe most in?"),
   antiAdvocate: window.prompt("What do you think should be reformed the most in the current state of the world?"),
   profession: window.prompt("What do you wanna be when you grow up?")
 };
 
-
+// user story
 
 const story = `${properties.userName}, who is a ${properties.profession} and a strident advocate for the cause of ${properties.advocate}, in their debate, is fighting against ${properties.biggestOpp}, in which ${properties.userName} is condemnding ${[properties.antiAdvocate]}. In their speech, located at ${properties.location}, ${properties.userName} asserts, "It must be noted that ${properties.advocate} is the right cause on the basis of moral judgement and well-being, and that ${properties.biggestOpp}'s beliefs of ${properties.antiAdvocate} is wrong, and they should be publicaly shamed." <br> <br> ${properties.biggestOpp}, ever the so stubborn opponet, scoffed at ${properties.userName}'s statment, claiming it as a "ludicrous evaluation disguised as an attempt to undermine their authority". Nevertheless, ${properties.userName} was victorious in the debate, since the federal government is now considering reforming ${properties.antiAdvocate}. However, supporters of ${properties.biggestOpp} are activley enganging in radical movements for ${properties.antiAdvocate}, though it has yet to make a big impact. All said and done, ${properties.userName} is the superior debator.`;
 
+//add the story to the container
 document.getElementById("madlib-output").innerHTML = story;
 
